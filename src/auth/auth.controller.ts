85cf6df13fe82c0adf489ceedb62024ebdb2fe2e
Gabryel <gabryel.macedo@gmail.com>
2025-05-29 21:47:43 -0300

    feat: implement login endpoint with LocalAuthGuard and logging

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Logger, Post, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './interfaces/auth-req.interface';
+import { LoginDto } from './dto/login.dto';
+import { Public } from './decorators/isPublic.decorator';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+    private readonly logger = new Logger(AuthController.name);
+    constructor(private readonly authService: AuthService) {}
+
+    @Public()
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    @HttpCode(200)
+    public async login(@Request() req: AuthRequest, @Body() loginDto: LoginDto): Promise<{ access_token: string }>  {
+        this.logger.debug(`AuthController.login: req.user recebido da LocalStrategy: ${JSON.stringify(req.user)}`);
+        return this.authService.login(req.user);
+    }
+}
