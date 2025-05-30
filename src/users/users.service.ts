fbebf1a88c1d89710739e0abfe684bd759ae314e
Gabryel <gabryel.macedo@gmail.com>
2025-05-30 16:32:01 -0300

    feat: implement update method to modify user details with validation and error handling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,8 +6,9 @@ import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { CreateUserDto } from './dto/create-user.dto';
 import { hash } from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
-import { GetCommand, PutCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { GetCommand, PutCommand, QueryCommand, UpdateCommand, UpdateCommandInput } from '@aws-sdk/lib-dynamodb';
 import { S3Service } from 'src/aws/s3/s3.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -139,4 +140,88 @@ export class UsersService {
             throw new InternalServerErrorException('An internal server error occurred while creating the user');
         }
     }
+
+    async update(userId: string, updateUserDto: UpdateUserDto): Promise<Omit<User, 'password'>> {
+    this.logger.log(`Updating user: ${userId}`);
+    const user = await this.findUserById(userId);
+    if (!user) {
+      this.logger.error(`User not found: ${userId}`);
+      throw new NotFoundException('User not found');
+    }
+
+    const updateExpressionParts: string[] = [];
+    const expressionAttributeValues: Record<string, any> = {};
+    const expressionAttributeNames: Record<string, string> = {};
+
+    expressionAttributeNames['#updatedAt'] = 'updatedAt';
+    expressionAttributeValues[':updatedAt'] = new Date().toISOString();
+    updateExpressionParts.push('#updatedAt = :updatedAt');
+    let changes = false;
+
+
+    if (updateUserDto.name && updateUserDto.name !== user.name) {
+      this.logger.log(`Updating name for user: ${userId}`);
+      updateExpressionParts.push('#name = :name');
+      expressionAttributeNames['#name'] = 'name';
+      expressionAttributeValues[':name'] = updateUserDto.name;
+      changes = true;
+    }
+
+    if(updateUserDto.phone && updateUserDto.phone !== user.phone) {
+      expressionAttributeValues[':phone'] = updateUserDto.phone;
+      updateExpressionParts.push('#phone = :phone');
+      changes = true;
+    }
+
+    if(updateUserDto.email && updateUserDto.email !== user.email) {
+      const existingUser = await this.findUserByEmail(updateUserDto.email);
+      if (existingUser) {
+        this.logger.error(`User with email ${updateUserDto.email} already exists.`);
+        throw new ConflictException(`This email is already in use`);
+      }
+      updateExpressionParts.push('email = :email');
+      expressionAttributeValues[':email'] = updateUserDto.email;
+      updateExpressionParts.push('isEmailValidated = :isEmailValidated');
+      expressionAttributeValues[':isEmailValidated'] = false;
+      changes = true;
+    }
+
+    if(updateUserDto.password) {
+      const saltRounds = 10;
+      const hashedPass = await hash(updateUserDto.password, saltRounds);
+      expressionAttributeNames['#password'] = 'password';
+      expressionAttributeValues[':password'] = hashedPass;
+      updateExpressionParts.push('#password = :password');
+      changes = true;
+    }
+  
+    if(!changes) {
+      this.logger.warn(`No changes detected for user: ${userId}`);
+      const { password, ...userResponse } = user;
+      return userResponse;
+    }
+
+    const updateCommandInput: UpdateCommandInput = {
+      TableName: this.tableName,
+      Key: { id: userId },
+      UpdateExpression: `SET ${updateExpressionParts.join(', ')}`,
+      ExpressionAttributeValues: expressionAttributeValues,
+      ReturnValues: 'ALL_NEW',
+    };
+
+    if(Object.keys(expressionAttributeNames).length > 0) {
+      updateCommandInput.ExpressionAttributeNames = expressionAttributeNames;
+    }
+
+    try {
+      const result = await this.dynamoDbService.docClient.send(new UpdateCommand(updateCommandInput));
+      this.logger.log(`User with ID ${userId} updated successfully in DynamoDB.`);
+      const { password, ...updatedUserWithoutPassword } = result.Attributes as User;
+      return updatedUserWithoutPassword;
+    } catch (error) {
+      this.logger.error(`Error updating user ${userId} in DynamoDB:`, error.stack);
+      throw new InternalServerErrorException('Failed to update user.');
+    }
+    
+  }
 }
