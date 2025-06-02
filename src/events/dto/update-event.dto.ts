f2464473d126a8b1bd3ab3c1b84660cd74bd660d
Gabryel <gabryel.macedo@gmail.com>
2025-06-02 16:15:54 -0300

    feat: add date validation to UpdateEventDto for correct format YYYY-MM-DD

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,12 +1,13 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreateEventDto } from "./create-event.dto";
-import { IsOptional, IsUUID } from "class-validator";
+import { IsDateString, IsOptional, IsUUID } from "class-validator";
 
 export class UpdateEventDto extends PartialType(CreateEventDto) {
     name?: string;
     
     description?: string;
     
+    @IsDateString({}, { message: 'Date must be in the correct format YYYY-MM-DD' })
     date?: string;
 
     @IsUUID('4', { message: 'Organizer ID must be a valid UUID' })
