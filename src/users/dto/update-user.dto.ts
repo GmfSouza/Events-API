c59ba4ca8105221cb05db842cd4bc2ef167c6e52
Gabryel <gabryel.macedo@gmail.com>
2025-06-02 00:26:12 -0300

    feat: add ApiProperty decorators to UpdateUserDto for improved Swagger documentation

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,30 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({
+    description: 'the new name of the user',
+    example: 'New Name',
+  })
   name?: string;
 
+  @ApiPropertyOptional({
+    description: 'the new email of the user. Must be a valid email format',
+    example: 'new.email@example.com',
+  })
   email?: string;
 
+  @ApiPropertyOptional({
+    description:
+      'the new password of the user. Must contain at least 8 characters, including letters, numbers, and special characters',
+    example: 'newpassword123!',
+  })
   password?: string;
 
+  @ApiPropertyOptional({
+    description:
+      'the new phone number of the user. Must be a valid phone number format',
+    example: '+55511912345678',
+  })
   phone?: string;
 }
