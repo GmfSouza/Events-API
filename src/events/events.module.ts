50e0978d1ca316f119d1569488dff747052e967a
Gabryel <gabryel.macedo@gmail.com>
2025-06-02 21:34:54 -0300

    feat: add MailModule to EventsModule imports

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -4,9 +4,10 @@ import { EventsService } from './events.service';
 import { AwsModule } from 'src/aws/aws.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [AwsModule, ConfigModule, UsersModule],
+  imports: [AwsModule, ConfigModule, UsersModule, MailModule],
   controllers: [EventsController],
   providers: [EventsService]
 })
