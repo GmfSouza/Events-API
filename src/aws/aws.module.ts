120e98112e0a487c178e7bbf704073b271bf772c
Gabryel <gabryel.macedo@gmail.com>
2025-06-04 15:04:50 -0300

    feat: add SeedModule to app module and remove unused SesService from AwsModule

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { S3Service } from './s3/s3.service';
-import { SesService } from './ses/ses.service';
 import { DynamoDbService } from './dynamodb/dynamodb.service';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [DynamoDbService, S3Service, SesService],
-  exports: [DynamoDbService, S3Service, SesService], 
+  providers: [DynamoDbService, S3Service],
+  exports: [DynamoDbService, S3Service], 
 })
 export class AwsModule {}
